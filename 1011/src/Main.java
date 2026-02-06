841ab487681a3beedace0fc2de80886f1138508b
G4M3RDR0ID1 <gamerdroid33@hotmail.com>
2026-02-06 20:29:27 -0300

    1011

---
+++ 1011/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int raio = sc.nextInt();
+
+        double formulaVolume = (4.0/3) * 3.14159 * Math.pow(raio, 3);
+
+        System.out.printf("VOLUME = %.3f", formulaVolume);
+    }
+
+}
